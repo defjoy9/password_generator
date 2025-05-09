4be249219e8a2637a4ceb761d02c312bf896665f
defjoy <defjoy9@users.noreply.github.com>
2025-05-09 21:42:22 +0200

    Generated password now shows up on screen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Password_Generator")
         self.setGeometry(700,300,500,500)
         self.line_edit = QLineEdit(self)
-        self.button = QPushButton("Copy Password",self)
+        self.button = QPushButton("Generate",self)
         self.uppercase = QCheckBox("Uppercase", self)
         self.lowercase = QCheckBox("Lowercase", self)
         self.numbers = QCheckBox("Numbers", self)
@@ -25,27 +25,31 @@ class MainWindow(QMainWindow):
         self.numbers.setGeometry(260,50,500,100)
         self.symbols.setGeometry(360,50,500,100)
 
-        self.uppercase.stateChanged.connect(self.checkbox_changed)
-        self.lowercase.stateChanged.connect(self.checkbox_changed)
-        self.numbers.stateChanged.connect(self.checkbox_changed)
-        self.symbols.stateChanged.connect(self.checkbox_changed)
+        self.button.clicked.connect(self.generate_password)
+        self.uppercase.stateChanged.connect(self.generate_password)
+        self.lowercase.stateChanged.connect(self.generate_password)
+        self.numbers.stateChanged.connect(self.generate_password)
+        self.symbols.stateChanged.connect(self.generate_password)
 
-    def checkbox_changed(self, state):
-        sender = self.sender()  # Gets the checkbox that sent the signal
+    def generate_password(self):
         all_chars = ""
-        if sender == self.uppercase:
-            if state == Qt.Checked:
-                all_chars += string.ascii_uppercase 
-        elif sender == self.lowercase:
-            if state == Qt.Checked:
-                all_chars += string.ascii_lowercase 
-        elif sender == self.numbers:
-            if state == Qt.Checked:
-                all_chars += string.digits 
-        elif sender == self.symbols:
-            if state == Qt.Checked:
-                all_chars += string.punctuation 
+        length = 16
 
+        if self.uppercase.isChecked():
+            all_chars += string.ascii_uppercase
+        if self.lowercase.isChecked():
+            all_chars += string.ascii_lowercase
+        if self.numbers.isChecked():
+            all_chars += string.digits
+        if self.symbols.isChecked():
+            all_chars += string.punctuation
+
+        if not all_chars:
+            self.line_edit.setText("Select at least one option.")
+            return
+
+        password = ''.join(random.choice(all_chars) for _ in range(length))
+        self.line_edit.setText(password)
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
@@ -55,33 +59,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# class Password_Generator:
-#     def __init__(self, length=12, uppercase=True, lowercase=True, number=True, symbols=True):
-#         self.length = length
-#         self.uppercase = uppercase
-#         self.lowercase = lowercase
-#         self.number = number
-#         self.symbols = symbols
-
-#     def generate(self):
-#         all_chars = ""
-
-#         if self.uppercase:
-#             all_chars += string.ascii_uppercase
-#         if self.lowercase:
-#             all_chars += string.ascii_lowercase
-#         if self.number:
-#             all_chars += string.digits
-#         if self.symbols:
-#             all_chars += string.punctuation
-
-#         password = ''.join(random.choice(all_chars) for _ in range(self.length))
-#         return password
-
-# def main():
-#     password_gen = Password_Generator(length=16, uppercase=True, lowercase=True, number=True, symbols=True)
-#     new_password = password_gen.generate()
-
-#     print(new_password)
